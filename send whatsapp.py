3cd2da803ca2c8a504090622bc745166db7e5393
Nesisa Moyo <moyonesisam@gmail.com>
2021-08-25 09:43:28 +0200

    Update send whatsapp.py

--- send whatsapp.py
+++ send whatsapp.py
@@ -9,7 +9,7 @@ auth_token = 'auth_token here'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-                              from_='whatsapp:twilio sandvine number here', 
+                              from_='whatsapp:twilio sandbox number here', 
                               //Whatsapp alert message containing picture of students improperly wearing masks and not weaaring masks
                               body='Mask Up App Alert!!! \n Student is entering without a face mask. \n See in camera to recognize student. \n Please advise to Mask Up!!',
                               media_url = 'http://6f470b37db77.ngrok.io/.jpeg',
